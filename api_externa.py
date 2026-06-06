d59ef8760cdbc4723a2657c80174d8d17a5d7322
HenriqueSena135 <riqueboy2006@hotmail.com>
2026-06-06 09:26:46 -0300

    testando api

---
+++ api_externa.py
@@ -0,0 +1,10 @@
+import requests as rq
+cep = input("Digite o CEP da sua cidade: ")
+
+url = f"https://viacep.com.br/ws/{cep}/json/"
+
+dados = rq.get(url)
+
+resposta = dados.json()
+
+print(f"o seu usuário mora na {resposta['logradouro']} no bairro {resposta['bairro']} no estado de {resposta['estado']} na região {resposta['regiao']} ")
